584b097c50f4da52e7f06321fc421b7080aa8ec9
Patricia Burns <patricia@tensoriot.com>
2020-10-17 17:16:52 -0400

    come back to useffect later

--- src/views/ApiCall.jsx
+++ src/views/ApiCall.jsx
@@ -5,29 +5,29 @@ import axios from "axios";
 const Example = () => {
   // this is a react hook and is the new way to use state within a component as of React 16
   const [data, setData] = React.useState([]);
-  let ref = React.useRef();
+  // const ref = React.useRef(true);
 
   // example of an api call using axios this was npm installed
   // taken from https://www.programmableweb.com/category/humor/api
+  // React.useEffect(() => {
+  //   () => (ref.current = false);
+  // }, []);
+
   const fetchFriendsData = async () => {
     try {
       const quotes = await axios.get("https://friends-quotes-api.herokuapp.com/quotes");
+      // if (ref.curent) {
+      // console.log(quotes);
       setData(quotes.data);
+      // }
     } catch (error) {
       console.log("error", error);
     }
   };
 
   React.useEffect(() => {
-    ref.current = true;
-    if (ref.current) {
-      fetchFriendsData();
-    }
-  }, [data]);
-
-  React.useEffect(() => {
-    return () => (ref.current = false);
-  });
+    fetchFriendsData();
+  }, []);
 
   if (!data.length) {
     return <div> Loading .... </div>;
