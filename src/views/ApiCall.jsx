6a11bd446ce302d5c6c81deebe5deef951e840b2
Patricia Burns <patricia@tensoriot.com>
2020-10-17 16:14:07 -0400

    need to fix useEffect;

--- src/views/ApiCall.jsx
+++ src/views/ApiCall.jsx
@@ -5,11 +5,12 @@ import axios from "axios";
 const Example = () => {
   // this is a react hook and is the new way to use state within a component as of React 16
   const [data, setData] = React.useState([]);
+  let ref = React.useRef();
 
-// example of an api call using axios this was npm installed 
-// taken from https://www.programmableweb.com/category/humor/api
+  // example of an api call using axios this was npm installed
+  // taken from https://www.programmableweb.com/category/humor/api
   React.useEffect(() => {
-    let mounted = true; 
+    ref.current = true;
     const fetchFriendsData = async () => {
       try {
         const quotes = await axios.get("https://friends-quotes-api.herokuapp.com/quotes");
@@ -17,23 +18,24 @@ const Example = () => {
       } catch (error) {
         console.log("error", error);
       }
-    }; 
-    if(mounted){
+    };
+    if (ref.current) {
       fetchFriendsData();
     }
-    return () => mounted = false; 
   }, [data]);
 
-  if(!data.length){
-    return(
-      <div> Loading .... </div>
-    )
+  React.useEffect(() => {
+    return () => (ref.current = false);
+  });
+
+  if (!data.length) {
+    return <div> Loading .... </div>;
   }
 
   return (
     <Paper>
       <div> Api Call Example</div>
-      <p> Taken from Friends,  `https://www.programmableweb.com/category/humor/api` </p>
+      <p> Taken from Friends, `https://www.programmableweb.com/category/humor/api` </p>
       {data.map((item, index) => {
         return (
           <div key={index}>
